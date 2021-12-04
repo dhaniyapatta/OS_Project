f5c2f6f1b8767be026276232b765836134a527e3
vardhanaleti <vardhanaleti2001@gmail.com>
2021-12-04 10:06:07 +0530

    added files

--- trial.c
+++ trial.c
@@ -7,6 +7,22 @@ const int PAGE_SIZE=256;
 const int TLB_SIZE=16;
 const int MM_SIZE=256;
 
+//shuffling
+void shuffle(int *array, size_t n)
+{
+    if (n > 1) 
+    {
+        size_t i;
+        for (i = 0; i < n - 1; i++) 
+        {
+          size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+          int t = array[j];
+          array[j] = array[i];
+          array[i] = t;
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
 	FILE *fd;
@@ -35,6 +51,18 @@ int main(int argc, char* argv[])
 
 	memset(tlb,-1,TLB_SIZE*2*sizeof(tlb[0][0]));
 	memset(pagetable,-1,sizeof(pagetable));
+
+    for(int i = 0; i < 256; i++)
+    {
+        pagetable[i] = i;
+    }
+    shuffle(pagetable,256);
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d\n",pagetable[i]);
+    }
+    printf("*****************\n");
+    //memset(pagetable,-1,sizeof(pagetable));
 	
 	int mask=255,mask1=62580,i,hit;
 	
@@ -50,13 +78,11 @@ int main(int argc, char* argv[])
 		offset=offset & mask;
 		
 		logicalad=atoi(value);
-		//printf("%lld %lld\n",page_no,offset);
 		frame=0,physicalad=0;
 		
 		hit=0;			//1 if found in TLB
-		
+		printf("%d is the Logical Address\n",logicalad);
 		//CHECK IN TLB
-		
 		for(i=0;i<TLB_SIZE;i++)
 		{
 			if(tlb[i][0]==page_no)
@@ -67,46 +93,24 @@ int main(int argc, char* argv[])
 				break;
 			}
 		}
-		//if present in tlb
-		if(hit==1)
-			printf("TLB HIT\n");
-		
-		//search in pagetable
-		else
-		{
-			
-			int f=0;
-			for(i=0;i<PAGE_SIZE;i++)
-			{
-				if(pagetable[i]==page_no)
-				{
-					frame=i;
-					fault++;
-					break;
-				}
-				if(pagetable[i]==-1)
-				{
-					f=1;
-					
-					break;
-				}
-			}
-			if(f==1)
-			{
-				pagetable[i]=page_no;
-				frame=i;
-			}
-			//replace in tlb using fifo
-			tlb[qp][0]=page_no;
-			tlb[qp][1]=i;
-			qp++;
-			qp=qp%15;		
-		}
+        //if present in tlb
+        if(hit == 1)
+            printf("HIT\n");
+        else
+        {
+            printf("Miss\n");
+            fault++;
+            frame = pagetable[page_no];
+            tlb[qp][0] = page_no;
+            tlb[qp][1] = frame;
+            qp++;
+            qp=qp%16;
+        }
 		if(logicalad<10000)
 		printf("VIRTUAL ADDRESS = %d \t\t\t",logicalad);
 		else
 		printf("VIRTUAL ADDRESS = %d \t\t",logicalad);
-		
+		printf("Page No: %lld and Offset: %lld\n",page_no,offset);
 		physicalad=frame*PAGE_SIZE + offset;
 		printf("PHYSICAL ADDRESS = %d\n",physicalad);
 	}
